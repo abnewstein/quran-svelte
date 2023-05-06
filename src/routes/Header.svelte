7f901921b0ae19559d21907340ae66e2fa121310
Abdul Rahman <abnewstein@gmail.com>
2023-05-06 18:19:16 -0400

    Initial commit

---
+++ src/routes/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+</script>
+
+<div class="navbar bg-secondary-100">
+	<div class="navbar-start">
+		<div class="dropdown">
+			<label tabindex="0" class="btn btn-ghost btn-circle" />
+			<ul
+				tabindex="0"
+				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-secondary-100 rounded-box w-52"
+			>
+				<li><a href="/chapter">Chapters</a></li>
+				<li><a href="/about">About</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="navbar-center">
+		<a href="/" class="btn btn-ghost normal-case text-xl">The Qur'an: A Complete Revelation</a>
+	</div>
+	<div class="navbar-end">
+		<button class="btn btn-ghost btn-circle" />
+	</div>
+</div>
+
+<style>
+</style>
