aa53ebbfd532a81f8f70674af80be63b2c3813fa
Abdul Rahman <abnewstein@gmail.com>
2023-05-07 00:10:10 -0400

    wip

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,10 +1,25 @@
-<script>
+<script lang="ts">
 </script>
 
 <div class="navbar bg-secondary-100">
 	<div class="navbar-start">
 		<div class="dropdown">
-			<label tabindex="0" class="btn btn-ghost btn-circle" />
+			<label tabindex="0" class="btn btn-ghost btn-circle">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25H12"
+					/>
+				</svg>
+			</label>
 			<ul
 				tabindex="0"
 				class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-secondary-100 rounded-box w-52"
@@ -15,11 +30,9 @@
 		</div>
 	</div>
 	<div class="navbar-center">
-		<a href="/" class="btn btn-ghost normal-case text-xl">The Qur'an: A Complete Revelation</a>
+		<a href="/" class="btn btn-ghost normal-case text-xl">Al Qur'an</a>
 	</div>
-	<div class="navbar-end">
-		<button class="btn btn-ghost btn-circle" />
-	</div>
+	<div class="navbar-end" />
 </div>
 
 <style>
