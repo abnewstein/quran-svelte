42c0f829edd819668cb434bf7935e282582a6eca
Abdul Rahman <abnewstein@gmail.com>
2023-05-13 03:51:29 -0400

    wip

---
+++ src/routes/chapter/[chapter]/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { quranDataStore, TranslationEnum } from '$lib/QuranData';
+	import { get } from 'svelte/store';
+
+	export let data;
+	const quranData = get(quranDataStore);
+	const chapterData = quranData?.getChapter(Number(data.chapterNumber));
+	const verseDataEn = quranData?.getVerses(data.chapterNumber, TranslationEnum.ENGLISH_SAM_GERRANS);
+	const verseDataAr = quranData?.getVerses(data.chapterNumber, TranslationEnum.ARABIC_ORIGINAL);
+</script>
+
+<div class="w-full">
+	{#each verseDataAr as verse}
+		<div key={verse.id} class="grid grid-cols-2 items-baseline gap-4 rounded p-1" />
+	{/each}
+</div>
+
+<style>
+</style>
