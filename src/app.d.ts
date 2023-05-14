52e75c3bc04014a883277ecc45c4f63104cda166
Abdul Rahman <abnewstein@gmail.com>
2023-05-14 02:01:16 -0400

    wip

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,11 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+import type { PrismaClient } from '@prisma/client';
+
 declare global {
+	// eslint-disable-next-line no-var
+	var prisma: PrismaClient | undefined;
+
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
