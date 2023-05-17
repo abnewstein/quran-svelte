3cc04579d85fc68fad11ed5a2b3b391eea287b35
Abdul Rahman <abnewstein@gmail.com>
2023-05-17 02:36:37 -0400

    wip

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,57 +5,51 @@
 <Navbar class="flex items-center">
 	<NavBrand class="">
 		<NavUl class="">
-			<NavLi>
-				<a href="/">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-5 w-5"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						><path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-						/>
-					</svg>
-				</a>
+			<NavLi href="/">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+					/>
+				</svg>
 			</NavLi>
-			<NavLi>
-				<a href="/chapter">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-6 h-6"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
-						/>
-					</svg>
-				</a>
+			<NavLi href="/chapter">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-6 h-6"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+					/>
+				</svg>
 			</NavLi>
-			<NavLi>
-				<a href="/about">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-5 w-5"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-						><path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-						/>
-					</svg>
-				</a>
+			<NavLi href="/about">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="h-5 w-5"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+					/>
+				</svg>
 			</NavLi>
 		</NavUl>
 	</NavBrand>
--- src/routes/chapter/+page.svelte
+++ src/routes/chapter/+page.svelte
@@ -8,27 +8,30 @@
 
 <div class="p-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-3">
 	{#each chapters as chapter}
-		<div
-			class="relative rounded-xl bg-white p-4 sm:p-6 lg:p-8 border-[0.0314rem] border-black shadow-md"
+		<a
+			class="chapter-card block relative rounded-xl bg-white p-4 sm:p-6 lg:p-8 border-[0.0314rem] border-black shadow-md"
+			href="/chapter/{chapter.number}"
 		>
-			<a class="block" href="/chapter/{chapter.number}">
-				<div class="flex flex-col items-center text-center mb-3">
-					<strong class="text-xl">{chapter.number}</strong>
-					<span class="mt-1 text-slate-600">
-						{chapter.name.arabic}
-						{'|'}
-						<small class="italic">{chapter.name.transliteration}</small>
-					</span>
-					<span class="mt-1">{chapter.name.english}</span>
-					<span
-						class="bottom-2 text-xs absolute prose prose-sm prose-stone border-black border-[0.0314rem] rounded-lg py-1 px-2"
-						>{chapter.versesCount} verses</span
-					>
-				</div>
-			</a>
-		</div>
+			<div class="flex flex-col items-center text-center mb-3">
+				<strong class="text-xl">{chapter.number}</strong>
+				<span class="mt-1 text-slate-600">
+					{chapter.name.arabic}
+					{'|'}
+					<small class="italic">{chapter.name.transliteration}</small>
+				</span>
+				<span class="mt-1">{chapter.name.english}</span>
+				<span
+					class="bottom-2 text-xs absolute prose prose-sm prose-stone border-black border-[0.0314rem] rounded-lg py-1 px-2"
+					>{chapter.versesCount} verses</span
+				>
+			</div>
+		</a>
 	{/each}
 </div>
 
 <style>
+	.chapter-card:hover {
+		box-shadow: 0 0 7px #1c1917;
+		transition: box-shadow 0.1s ease-in-out;
+	}
 </style>
