77462a63be37c373dfb7b3de351c82b11d2d937e
Abdul Rahman <abnewstein@gmail.com>
2023-05-26 18:15:45 -0400

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,18 @@
 	import 'uno.css';
 	import './styles.css';
 	import Header from '$lib/components/Header.svelte';
+	import { fade } from 'svelte/transition';
+
+	export let data;
 </script>
 
 <div class="app">
 	<Header />
-	<main>
-		<slot />
-	</main>
+	{#key data.pathname}
+		<main in:fade={{ duration: 100, delay: 100 }} out:fade={{ duration: 100 }}>
+			<slot />
+		</main>
+	{/key}
 </div>
 
 <style>
